ec176596eea3611d3f8b37dd21d7050b3c653644
Alejandro Matteucci <xaos27@gmail.com>
2026-05-12 15:47:56 -0400

    fix: redirect vender-cupo-internacional a cupo-internacional-a-pesos via vercel routes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import VentaUSD from './pages/VentaUSD.tsx';
 import PSEOPage from './pages/PSEOPage.tsx';
@@ -10,9 +10,6 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/venta-usd" element={<VentaUSD />} />
-        {/* Redirects */}
-        <Route path="/vender-cupo-internacional" element={<Navigate to="/cupo-internacional-a-pesos" replace />} />
-        {/* Catch-all PSEO */}
         <Route path="/:slug" element={<PSEOPage />} />
       </Routes>
     </BrowserRouter>
